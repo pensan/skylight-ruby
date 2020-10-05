f35aec795da76aaa750e7d5fa2a4ceb23d53d459
Philipp Goetz <pensan@users.noreply.github.com>
2020-10-05 13:43:35 +0200

    Cast env variable to float

--- lib/skylight/middleware.rb
+++ lib/skylight/middleware.rb
@@ -80,7 +80,7 @@ module Skylight
         error "Already instrumenting. Make sure the Skylight Rack Middleware hasn't been added more than once."
       end
 
-      threshold = env["SKYLIGHT_THRESHOLD"] || 0.001
+      threshold = env["SKYLIGHT_THRESHOLD"].to_f || 0.001
                                          # Reduce number of requests tracked
       if env["REQUEST_METHOD"] == "HEAD" || rand > threshold
         t { "middleware skipping HEAD" }
